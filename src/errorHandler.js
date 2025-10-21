5d80a8e17b7940c39e14a33ff9f9a9c4b8ef7365
Your Name <iriguille0504@gmail.com>
2025-10-20 22:52:31 -0300

    arreglo provisorio de errorHandler *ARREGLAR

--- src/errorHandler.js
+++ src/errorHandler.js
@@ -1,4 +1,4 @@
-exports.showErrorMessage = (httpErrorCode, message) => {
+/* const showErrorMessage = (httpErrorCode, message) => {
   const imageUrl = `https://http.cat/images/${code}.jpg`;
   const errorPayload = {
     status: code,
@@ -7,5 +7,20 @@ exports.showErrorMessage = (httpErrorCode, message) => {
   };
   console.log("{message}", errorPayload);
 }
+
+module.exports = { showErrorMessage }; */
+
+const showErrorMessage = (httpErrorCode = 500, message = "") => {
+  const code = Number(httpErrorCode) || 500;
+  const imageUrl = `https://http.cat/${code}.jpg`;
+  const errorPayload = {
+    status: code,
+    message: String(message),
+    imageUrl
+  };
+  console.log("[errorHandler]", errorPayload);
+  return errorPayload;
+};
+// ...existing code...
 
 module.exports = { showErrorMessage };

e4a518510bddce10e1d78c6f4eb148262e7d665b
Your Name <iriguille0504@gmail.com>
2025-11-02 18:29:15 -0300

    usuarioRoutes y jornadaRoutes added in authDictionary

--- src/authDictionary.js
+++ src/authDictionary.js
@@ -1,35 +1,35 @@
 diccionarioRutasYPermisos = { //es para discriminar entre si sos "juegoteka" o "jugador". Si puede cualquiera de los 2, no incluir dicha ruta
     //RUTAS DE juegoRoutes.js
     "/juegos/create": ["juegoteka", "administrador"],
-    "/juegos/update/:id": "juegoteka",
-    "/juegos/delete/:id": "juegoteka",
-    "/juegos/delete/hard/:id": "juegoteka",
+    "/juegos/update/:id": ["juegoteka", "administrador"],
+    "/juegos/delete/:id": ["administrador"],
+    "/juegos/delete/hard/:id": ["administrador"],
 
     // RUTAS DE usuarioRoutes.js
-    //"/usuarios/": "", //todos
-    //"/usuarios/edit": "", //todos
-   // "/usuarios/registrar": "", //todos
-    //"/usuarios/login": "", //todos
-   // "/usuarios/getPerfil": "", //todos
-   // "/usuarios/getId/:id": "", //todos
-    //"/usuarios/getUsername/:userName": "", //todos
-   // "/usuarios/cambiarPassword": "", //todos
-   // "/usuarios/getJuegotekas": "", //todos
-   // "/usuarios/getJugadores": "", //todos
+    "/usuarios/": [], //todos
+    "/usuarios/edit": [], //todos
+    "/usuarios/registrar": [], //todos
+    "/usuarios/login": [], //todos
+    "/usuarios/getPerfil": [], //todos
+    "/usuarios/getId/:id": [], //todos
+    "/usuarios/getUsername/:userName": [], //todos
+    "/usuarios/cambiarPassword": [], //todos
+    "/usuarios/getJuegotekas": [], //todos
+    "/usuarios/getJugadores": [], //todos
    //Tienen los dos siguientes: isNotAdmin 
-   //"/usuarios/misJuegos": "", //juegoteka,jugador
-   // "/usuarios/misJuegos/:idJuego": "", //juegoteka,jugador
+    "/usuarios/misJuegos": ["juegoteka", "jugador"], //juegoteka,jugador
+    "/usuarios/misJuegos/:idJuego": ["juegoteka", "jugador"], //juegoteka,jugador
 
 
 
     // RUTAS DE jornadaRoutes.js
-    //"/jornadas/": "", //todos
-    //"/jornadas/:id": "", //todos
-    //"/jornadas/create": "", //juegoteka NO ADMIN
-    "/jornadas/edit/:id": "juegoteka", //juegoteka - ADMIN
-   // "/jornadas/updateEncuentros/:id": "", //jugador, juegoteka NO ADMIN
-    //"/jornadas/inscripcion/:id": "", //jugador NO ADMIN
-    "/jornadas/updateEstado/:id": "juegoteka", //juegoteka - ADMIN
+    "/jornadas/": [], //todos
+    "/jornadas/:id": [], //todos
+    "/jornadas/create": ["juegoteka"], //juegoteka NO ADMIN
+    "/jornadas/edit/:id": ["juegoteka", "administrador"], //juegoteka - ADMIN
+    "/jornadas/updateEncuentros/:id": ["jugador", "juegoteka"], //jugador, juegoteka NO ADMIN
+    "/jornadas/inscripcion/:id": ["jugador"], //jugador NO ADMIN
+    "/jornadas/updateEstado/:id": ["juegoteka", "administrador"], //juegoteka - ADMIN
 
   // RUTAS DE encuentroRoutes.js
     "/encuentros": "",

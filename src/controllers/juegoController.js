b22652a784a5b49650fecb96d5829c44795d83ff
Your Name <iriguille0504@gmail.com>
2025-11-02 17:20:15 -0300

    updates en archivos de juegos (Service, model, controller)

--- src/controllers/juegoController.js
+++ src/controllers/juegoController.js
@@ -5,7 +5,7 @@ const Juego = require("../models/juegoModel");
 
 exports.getAllJuegos = async (req, res) => {
   try {
-    const juegos = await Juego.find();
+    const juegos = await Juego.find({estado : "activo"});
     res.json(juegos);
   } catch (err){
     showErrorMessage(res, 500, "No se obtuvo la lista de juegos");
@@ -16,7 +16,7 @@ exports.getJuegoById = async (req, res) => {
   try {
     const juego = await Juego.findById(req.params.id); //lo trae del modelo
     juego //ternario IF
-    ? res.json(juego) //true
+    ? juego.estado === "activo" ? res.json(juego) : showErrorMessage(res, 404, "Juego eliminado, no existe mas :(") //false
     : showErrorMessage(res, 404, "Juego no encontrado"); //false
   } catch (err){
     showErrorMessage(res, 500, "Error al obtener juego");
@@ -25,14 +25,15 @@ exports.getJuegoById = async (req, res) => {
 
 exports.createJuego = async (req, res) => {
   try {
-    const { titulo, imagen, descripcion, reglamento, cantJugadoresMax, cantJugadoresMin, tiempoEstimado } = req.body;
-    if (!titulo || !imagen || !descripcion || !reglamento || !cantJugadoresMax || !cantJugadoresMin || !tiempoEstimado){
-      res.status(400).json({ error: "Faltan campos obligatorios" });
+    const { titulo, imagen, descripcion, reglamento, cantJugadoresMax, cantJugadoresMin, tiempoEstimado, estado } = req.body;
+    if (!titulo || !imagen || !descripcion || !reglamento || !cantJugadoresMax || !cantJugadoresMin || !tiempoEstimado || !estado){
+      showErrorMessage(res, 400, "Faltan campos obligatorios");
+    } else {
+      const juegoCreado = await Juego.create({ titulo, imagen, descripcion, reglamento, cantJugadoresMax, cantJugadoresMin, tiempoEstimado, estado });
+      res.json(juegoCreado)
     }
-    const juegoCreado = await Juego.create({ titulo, imagen, descripcion, reglamento, cantJugadoresMax, cantJugadoresMin, tiempoEstimado });
-    res.json(juegoCreado)
   } catch (err){
-    res.status(500).json({ error: "Error al crear juego" });
+    showErrorMessage(res, 500, "Error al crear juego");
   }
 };
 
@@ -41,13 +42,41 @@ exports.updateJuego = async (req, res) => {
     const juegoActualizado = await Juego.findByIdAndUpdate(req.params.id, req.body, { new: true }); //traer el body p/updatear
     juegoActualizado //ternario IF
     ? res.json(juegoActualizado) //true
-    : res.status(404).json({ error: "Juego no encontrado" }); //false
+    : showErrorMessage(res, 500, "Juego no encontrado"); //false
   } catch (err){
-    res.status(500).json({ error: "Error al actualizar juego" });
+    showErrorMessage(res, 500, "Error al actualizar juego");
+  }
+};
+
+exports.updateJuego = async (req, res) => {
+  try {
+    const juego = await Juego.findById(req.params.id);
+    if (!juego) {
+      showErrorMessage(res, 404, "Juego no encontrado");
+    }
+    if (juego.estado !== "activo") {
+      showErrorMessage(res, 400, "No se puede actualizar un juego eliminado");
+    } else {
+      const juegoActualizado = await Juego.findByIdAndUpdate( req.params.id, req.body, { new: true });
+      res.json(juegoActualizado);
+    }
+  } catch (err) {
+    showErrorMessage(res, 500, "Error al actualizar juego");
   }
 };
 
 exports.deleteJuego = async (req, res) => {
+  try {
+    const juegoBorrado = await Juego.findByIdAndUpdate(req.params.id,{estado: "eliminado"}, {new: true}); //el new:true trae el documento ya actualizado.
+    juegoBorrado //ternario IF
+    ? res.json(juegoBorrado) //true
+    : showErrorMessage(res, 404, "Juego no encontrado, que triste no?");
+  } catch (err){
+    showErrorMessage(res, 500, "Error al borrar juego");
+  }
+};
+/*
+exports.deleteJuegoHARD = async (req, res) => {
   try {
     const juegoBorrado = await Juego.findByIdAndDelete(req.params.id); //lo trae del modelo
     juegoBorrado //ternario IF
@@ -57,10 +86,10 @@ exports.deleteJuego = async (req, res) => {
     showErrorMessage(res, 500, "Error al borrar juego");
   }
 };
-
+*/
 exports.getJuegoPorNombre = async (req, res) => {
   try {
-    const juegoBuscado = await Juego.findOne({ titulo: req.params.nombre}); //lo trae del modelo
+    const juegoBuscado = await Juego.findOne({ titulo: req.params.nombre}, {estado: "activo"}); //lo trae del modelo
     juegoBuscado //ternario IF
     ? res.json(juegoBuscado) //true
     : showErrorMessage(res, 404, "Juego no encontrado, que triste no?");
